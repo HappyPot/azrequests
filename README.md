723c0aecea8329991f715023f68f5e1799359728
wanglei <rrwanglei@126.com>
2019-04-15 17:06:14 +0800

    add source file

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# azrequests
-针对公司的业务流程封装的请求插件
 ###在组件中的使用方式
 ```
 let azrequests = require('azrequests')
 new azrequests(url,param,type).handlData(method,callback(res,err))
 ```
-###api
+###API
 | 变量名 | 描述 | 默认值 |
 | :------| ------: | :------: |
 | url | 接口地址 | String ：例如“https://api.apiopen.top/recommendPoetry” |
---
+++ index.js
@@ -0,0 +1,74 @@
+let axios = require('axios')
+class Request {
+    constructor(url, param, type = "JSON") {
+        this.url = url
+        this.param = param
+        this.flag = true
+        this.type = type
+        this.resData = {}
+    }
+    post(success, err) {
+        if (this.type == 'formData') {
+            var $param = new FormData()
+            for (let key in this.param) {
+                if (Object.prototype.toString.call(this.param[key]) === '[object Object]') {
+                    $param.append(key, JSON.stringify(this.param[key]))
+                } else {
+                    $param.append(key, this.param[key])
+                }
+            }
+            this.param = $param
+        }
+        axios({
+            method: "post",
+            url: this.url,
+            data: this.param != null ? this.param : ''
+        }).then(res => {
+            this.resData = res
+            if (res.data.code > 0) {
+                this.flag = true
+                success(res)
+            } else {
+                if (flag) {
+                    err(res.data.msg)
+                    this.flag = false
+                    return false;
+                }
+            }
+        }).catch(err => {
+            if (this.resData) {
+                err(this.resData.data.code, this.resData.data.msg)
+            } else {
+                err(null, '服务器错误请稍后再试!')
+            }
+        })
+    }
+    get(success, err) {
+        axios({
+            method: "get",
+            url: this.url,
+            data: this.param != null ? this.param : ''
+        }).then(res => {
+            success(res);
+        }).catch(error => {
+            err(error)
+        })
+    }
+    handlData(method, callback) {
+        if (method.toLowerCase() == 'post') {
+            this.post((res) => {
+                callback && callback(res)
+            }, (err, msg) => {
+                callback && callback(err, msg)
+            })
+        } else {
+            this.get(res => {
+                callback && callback(res)
+            }, (err, msg) => {
+                callback && callback(err, msg)
+            })
+        }
+    }
+}
+
+module.exports = Request
